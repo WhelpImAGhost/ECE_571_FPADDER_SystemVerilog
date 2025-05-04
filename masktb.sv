14fe33a1e941e08f625a8d46c6ba5d2e90995aa9
chriskp-pdx <chrisk@pdx.edu>
2025-05-03 19:07:17 -0700

    Update masktb.sv

--- masktb.sv
+++ masktb.sv
@@ -1,6 +1,6 @@
 //Test Bench to Ensure Proper Partitioning of 32-Bit FP Values
 
-module top();
+module top;
 
     bit [31:0] A, B;                        //Inputs A & B
     bit signA, signB;                       //Output Sign Bits
@@ -11,20 +11,22 @@ module top();
 
     initial
     begin
-        repeat (1024)
+        repeat (1024) #10;
+        A = $random;
+        B = $random;
         begin
             //Sign Error Check
             if ((signA !== A >> 31) || (signB !== B >> 31))             
                 $display("Improper Extraction of Sign Bit for test A = %p B=%p\nExpected Sign A = %p, Calculated Sign A = %p\nExpected Sign B = %p, Calculated Sign B = %p",
                 A, B, A >> 31, signA, B >> 31, signB);
             //Exponent Error Check
-            if ((exponentA !== ((A >> 24) & 2'hFF)) || (exponentB !== ((B >> 24) & 2'hFF)))
+            if ((exponentA !== ((A >> 24) & 8'hFF)) || (exponentB !== ((B >> 24) & 8'hFF)))
                 $display("Improper Extraction of Exponent for test A = %p B=%p\nExpected Exponent A = %p, Calculated Exponent A = %p\nExpected Exponent B = %p, Calculated Exponent B = %p",
-                A, B, (A >> 24) & 2'hFF, exponentA, (B >> 24) & 2'hFF, exponentB);
+                A, B, (A >> 24) & 8'hFF, exponentA, (B >> 24) & 8'hFF, exponentB);
             //Mantissa Error Check
-            if ((mantissaA !== (A & 6'h7FFFFF)) || (mantissaB !== (B & 6'h7FFFFF)))
+            if ((mantissaA !== (A & 23'h7FFFFF)) || (mantissaB !== (B & 23'h7FFFFF)))
                 $display("Improper Extraction of Mantissa for testA = %p B=%p\nExpected Mantissa A = %p, Calculated Mantissa A = %p\nExpected Mantissa B = %p, Calculated Mantissa B = %p",
-                A, B, (A & 6'h7FFFFF), mantissaA, (B & 6'h7FFFFF), mantissaB);
+                A, B, (A & 23'h7FFFFF), mantissaA, (B & 23'h7FFFFF), mantissaB);
         end
 
         $display("Simulation Finished");
