aa281d313d85ff076534492d15e3bc268ede7325
Violet Monti <monti@mo.ece.pdx.edu>
2025-05-03 18:36:57 -0700

    working module and tb

--- fpaddertb.sv
+++ fpaddertb.sv
@@ -18,15 +18,18 @@ FPAdder f0 (unionA.bits, unionB.bits, unionX.bits);
 initial
 begin
 
-	unionA.f = 1.245;
+	unionA.f = -1.245;
 	unionB.f = 2.753;
     #10;
+    `ifdef DEBUGTB
+        $display("Adding %e and %e resulted in %e, expected is %e ", unionA.f, unionB.f, unionX.f, unionA.f + unionB.f);
+    `endif
 
     if ( unionX.f !== unionA.f + unionB.f)
     begin
         error++;
+	$display("Adding %e and %e resulted in %e instead of %e ", unionA.f, unionB.f, unionX.f, unionA.f + unionB.f);
         $display("FP Adder failed static case. Test Failed");
-        $display("Adding %f and %f resulted in %f instead of %f ", unionA.f, unionB.f, unionX.f, unionA.f + unionB.f);
         $finish;
     end
 
@@ -36,14 +39,19 @@ begin
 
 	do  rawA = $urandom;  while (rawA[30:23] == 8'hFF); // avoid Inf/NaN
 	do  rawB = $urandom;  while (rawB[30:23] == 8'hFF);
-	unionA.f = shortreal'(rawA);
-	unionB.f = shortreal'(rawB);
+	unionA.bits = rawA;
+	unionB.bits = rawB;
         
 	#10
+    `ifdef DEBUGTB
+        $display("Adding %e and %e resulted in %e, expected is %e ", unionA.f, unionB.f, unionX.f, unionA.f + unionB.f);
+	    $display("A: %b		B: %b		Act: %b		Ex: %b", unionA.bits, unionB.bits, unionX.bits, unionA.bits + unionB.bits );
+
+    `endif
         if ( unionX.f !== unionA.f + unionB.f)
         begin
             error++;
-            $display("FP adder failed. Adding %f and %f resulted in %f instead of %f ", unionA.f, unionB.f, unionX.f, unionA.f + unionB.f);
+            $display("FP adder failed. Adding %e and %e resulted in %e instead of %e ", unionA.f, unionB.f, unionX.f, unionA.f + unionB.f);
 	    $display("A: %b		B: %b		Act: %b		Ex: %b", unionA.bits, unionB.bits, unionX.bits, unionA.bits + unionB.bits );
         end
     end
