9011f6c59971a327f8564d5aad4c30fe3e210067
chriskp-pdx <chrisk@pdx.edu>
2025-05-03 19:13:00 -0700

    Fixed some Bugs in masktb

--- masktb.sv
+++ masktb.sv
@@ -11,21 +11,22 @@ module top;
 
     initial
     begin
-        repeat (1024) #10;
-        A = $random;
-        B = $random;
+        repeat (1024)
         begin
+            #10;
+            A = $random;
+            B = $random;
             //Sign Error Check
             if ((signA !== A >> 31) || (signB !== B >> 31))             
-                $display("Improper Extraction of Sign Bit for test A = %p B=%p\nExpected Sign A = %p, Calculated Sign A = %p\nExpected Sign B = %p, Calculated Sign B = %p",
+                $display("Improper Extraction of Sign Bit for test A = %h B=%h\nExpected Sign A = %h, Calculated Sign A = %h\nExpected Sign B = %h, Calculated Sign B = %h",
                 A, B, A >> 31, signA, B >> 31, signB);
             //Exponent Error Check
-            if ((exponentA !== ((A >> 24) & 8'hFF)) || (exponentB !== ((B >> 24) & 8'hFF)))
-                $display("Improper Extraction of Exponent for test A = %p B=%p\nExpected Exponent A = %p, Calculated Exponent A = %p\nExpected Exponent B = %p, Calculated Exponent B = %p",
-                A, B, (A >> 24) & 8'hFF, exponentA, (B >> 24) & 8'hFF, exponentB);
+            if ((exponentA !== ((A >> 23) & 8'hFF)) || (exponentB !== ((B >> 23) & 8'hFF)))
+                $display("Improper Extraction of Exponent for test A = %h B=%h\nExpected Exponent A = %h, Calculated Exponent A = %h\nExpected Exponent B = %h, Calculated Exponent B = %p",
+                A, B, (A >> 23) & 8'hFF, exponentA, (B >> 23) & 8'hFF, exponentB);
             //Mantissa Error Check
             if ((mantissaA !== (A & 23'h7FFFFF)) || (mantissaB !== (B & 23'h7FFFFF)))
-                $display("Improper Extraction of Mantissa for testA = %p B=%p\nExpected Mantissa A = %p, Calculated Mantissa A = %p\nExpected Mantissa B = %p, Calculated Mantissa B = %p",
+                $display("Improper Extraction of Mantissa for test A = %h B=%h\nExpected Mantissa A = %h, Calculated Mantissa A = %h\nExpected Mantissa B = %h, Calculated Mantissa B = %p",
                 A, B, (A & 23'h7FFFFF), mantissaA, (B & 23'h7FFFFF), mantissaB);
         end
 
