e6bf9b582d5d38020a9f2fe84f64d439ce6861cd
chriskp-pdx <chrisk@pdx.edu>
2025-05-08 22:50:14 -0700

    Update alu.sv

--- alu.sv
+++ alu.sv
@@ -4,12 +4,15 @@ module ALU(fpbus.alu bus);
 
     always_comb 
     begin
-        if (bus.signA == bus.signB)     {bus.carryOut,bus.alignedResult} = bus.alignedMantissaA + bus.alignedMantissaB;
+        if (bus.signA == bus.signB)
+            {bus.carryOut,bus.alignedResult} = bus.alignedMantissaA + bus.alignedMantissaB;
         else 
         begin
-                                        bus.carryOut = 0;
-            if (bus.signA > bus.signB)  bus.alignedResult = bus.alignedMantissaA - bus.alignedMantissaB;
-            else                        bus.alignedResult = bus.alignedMantissaB - bus.alignedMantissaA;
+            bus.carryOut = 0;
+            if (bus.signA > bus.signB)
+                bus.alignedResult = bus.alignedMantissaA - bus.alignedMantissaB;
+            else
+                bus.alignedResult = bus.alignedMantissaB - bus.alignedMantissaA;
         end
     end
 
