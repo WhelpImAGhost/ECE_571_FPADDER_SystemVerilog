eab0e2385a2a3fd77b98b7d7e908d157187ef3be
chriskp-pdx <chrisk@pdx.edu>
2025-05-08 21:46:21 -0700

    Update mask.sv

--- mask.sv
+++ mask.sv
@@ -1,20 +1,15 @@
 //Module to Unpack Inputs Into Correct Field
 
-module Mask(
-    input logic [31:0] A, B,
-    output logic signA, signB,
-    output logic [7:0] exponentA, exponentB,
-    output logic [22:0] mantissaA, mantissaB
-);
+module Mask(fpbus.mask bus);
 
     always_comb
     begin
-        signA = A [31];         //Sign Bit for Addend "A"
-        signB = B [31];         //Sign Bit for Addend "B"
-        exponentA = A [30:23];  //8-Bit Exponent Field for Addend "A"
-        exponentB = B [30:23];  //8-Bit Exponent Field for Addend "B"
-        mantissaA = A [22:0];   //23-Bit Exponent Field for Addend "A"
-        mantissaB = B [22:0];   //23-Bit Exponent Field for Addend "B"
+        bus.signA = bus.A [31];         //Sign Bit for Addend "A"
+        bus.signB = bus.B [31];         //Sign Bit for Addend "B"
+        bus.exponentA = bus.A [30:23];  //8-Bit Exponent Field for Addend "A"
+        bus.exponentB = bus.B [30:23];  //8-Bit Exponent Field for Addend "B"
+        bus.mantissaA = bus.A [22:0];   //23-Bit Exponent Field for Addend "A"
+        bus.mantissaB = bus.B [22:0];   //23-Bit Exponent Field for Addend "B"
     end
 
 endmodule
