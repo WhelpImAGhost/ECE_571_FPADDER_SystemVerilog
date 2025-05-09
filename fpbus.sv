29f8f6756e3c4766ce8c6fb9d45de8b3ae7fe50f
Violet Monti <76889116+WhelpImAGhost@users.noreply.github.com>
2025-05-08 21:14:31 -0700

    create fpbus

---
+++ fpbus.sv
@@ -0,0 +1,22 @@
+interface fpbus;
+    logic [31:0] A, B, Result;
+    logic signA, signB;
+    logic [7:0] exponentA, exponentB, exponentOut;
+    logic [22:0] mantissaA, mantissaB;
+    logic [23:0] alignedMantissaA, alignedMantissaB;
+
+    modport mask (input A, B,
+                  output signA, signB,
+                  output exponentA, exponentB,
+                  output mantissaA, mantissaB );
+
+    modport align (input exponentA, exponentB,
+                   input mantissaA, mantissaB,
+                   output alignedMantissaA, alignedMantissaB,
+                   output exponentOut);
+
+    modport bench (input Result,
+                   output A, B);
+
+
+endinterface
