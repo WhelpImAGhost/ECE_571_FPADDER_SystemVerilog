ef6a4a991cd955185959abdee08d5ceaa6f16db1
chriskp-pdx <chrisk@pdx.edu>
2025-05-15 20:34:12 -0700

    Update alu.sv

--- alu.sv
+++ alu.sv
@@ -14,12 +14,12 @@ module ALU(fpbus.alu bus);
             bus.carryOut = 0;
             if (bus.alignedMantissaA > bus.alignedMantissaB)
             begin
-                {bus.carryOut, bus.alignedResult} = bus.alignedMantissaA - bus.alignedMantissaB;
+                bus.alignedResult = bus.alignedMantissaA - bus.alignedMantissaB;
                 bus.alignedSign = bus.signA;
             end
             else
             begin
-                {bus.carryOut, bus.alignedResult} = bus.alignedMantissaB - bus.alignedMantissaA;
+                bus.alignedResult = bus.alignedMantissaB - bus.alignedMantissaA;
                 bus.alignedSign = bus.signB;
             end
         end
