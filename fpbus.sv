6c91694b5ea01d048ec6ba1fb7475f695c22198a
chriskp-pdx <chrisk@pdx.edu>
2025-05-17 13:02:31 -0700

    Updated fpbus to add additional rounding signals

--- fpbus.sv
+++ fpbus.sv
@@ -1,28 +1,30 @@
 interface fpbus;
     logic [31:0] A, B, Result;
-    logic signA, signB, alignedSign, carryOut, normalizedSign;
+    logic signA, signB, alignedSign, carryOut, normalizedSign, guardBit, roundBit, stickyBit;
     logic [7:0] exponentA, exponentB, exponentOut, normalizedExponent;
     logic [22:0] mantissaA, mantissaB, normalizedMantissa;
     logic [23:0] alignedMantissaA, alignedMantissaB, alignedResult;
 
-    modport mask (input A, B,
-                  output signA, signB,
-                  output exponentA, exponentB,
-                  output mantissaA, mantissaB );
+    modport mask (  input A, B,
+                    output signA, signB,
+                    output exponentA, exponentB,
+                    output mantissaA, mantissaB);
 
-    modport align (input exponentA, exponentB,
-                   input mantissaA, mantissaB,
-                   output alignedMantissaA, alignedMantissaB,
-                   output exponentOut);
+    modport align ( input exponentA, exponentB,
+                    input mantissaA, mantissaB,
+                    output guardBit, roundBit, stickyBit,
+                    output alignedMantissaA, alignedMantissaB,
+                    output exponentOut);
 
-    modport alu (input signA, signB,
-                 input alignedMantissaA, alignedMantissaB,
-                 output alignedResult, alignedSign, carryOut);
+    modport alu (   input signA, signB,
+                    input alignedMantissaA, alignedMantissaB,
+                    output alignedResult, alignedSign, carryOut);
 
     modport normal (input alignedResult, exponentOut, alignedSign,
-        output normalizedSign, normalizedExponent, normalizedMantissa);
+                    input guardBit, roundBit, stickyBit,
+                    output normalizedSign, normalizedExponent, normalizedMantissa);
 
-    modport pack (input normalizedSign, normalizedExponent, normalizedMantissa,
-        output Result);
+    modport pack (  input normalizedSign, normalizedExponent, normalizedMantissa,
+                    output Result);
 
 endinterface
