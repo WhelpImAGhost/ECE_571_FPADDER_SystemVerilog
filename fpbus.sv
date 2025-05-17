f9529a111b920b5dbccc226884d2870d9ae6f9cc
chriskp-pdx <chrisk@pdx.edu>
2025-05-17 16:31:32 -0700

    Update fpbus.sv

--- fpbus.sv
+++ fpbus.sv
@@ -1,7 +1,7 @@
 interface fpbus;
     logic [31:0] A, B, Result;
     logic signA, signB, alignedSign, carryOut, normalizedSign, 
-    logic stickyBit, guardBit, roundBit;
+    logic stickyBit, guardBit, roundBit,
     logic [7:0] exponentA, exponentB, exponentOut, normalizedExponent;
     logic [22:0] mantissaA, mantissaB, normalizedMantissa;
     logic [23:0] alignedMantissaA, alignedMantissaB, alignedResult;
