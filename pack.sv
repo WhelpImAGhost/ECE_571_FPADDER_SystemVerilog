f446451802525817f261582db73f08ba54031108
chriskp-pdx <chrisk@pdx.edu>
2025-05-25 02:27:35 -0700

    Update pack.sv

--- pack.sv
+++ pack.sv
@@ -1,4 +1,12 @@
 //Module to Repack Result into IEEE-754 Standardized Format
 module Pack(fpbus.pack bus);
     assign bus.Result = {bus.normalizedSign, bus.normalizedExponent, bus.normalizedMantissa};
+
+    `ifdef FULLDEBUG
+        `define DEBUGPACK
+    `endif
+
+    `ifdef DEBUGPACK
+        //enter display here
+    `endif
 endmodule
