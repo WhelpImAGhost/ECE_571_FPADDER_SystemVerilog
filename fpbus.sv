72f3503bf17294f3862df0e83141e23e0723c05e
chriskp-pdx <chrisk@pdx.edu>
2025-05-24 21:29:12 -0700

    Update fpbus.sv

--- fpbus.sv
+++ fpbus.sv
@@ -15,34 +15,33 @@ interface fpbus;
     logic Ainf, Binf, ANaN, BNaN, Asub, Bsub, Azero, Bzero, Anormal, Bnormal;
     logic bypassALU, Aex, Bex;
 
-    modport mask (  input A, B,
+    modport mask(   input A, B,
                     output signA, signB,
                     output exponentA, exponentB,
                     output mantissaA, mantissaB,
                     output Ainf, ANaN, Asub, Azero, Anormal,
                     output Binf, BNaN, Bsub, Bzero, Bnormal);
 
-    modport align ( input exponentA, exponentB,
+    modport align(  input exponentA, exponentB,
                     input mantissaA, mantissaB,
-                    input signA, signB,
                     input Ainf, ANaN, Asub, Azero, Anormal,
                     input Binf, BNaN, Bsub, Bzero, Bnormal
                     output alignedMantissaA, alignedMantissaB,
                     output exponentOut,
                     output bypassALU, Aex, Bex);
 
-    modport alu (   input signA, signB, exponentA, exponentB,
+    modport alu(    input signA, signB,
                     input alignedMantissaA, alignedMantissaB,
-                    input bypassALU, Aex, Bex,
+                    input bypassALU,
                     output alignedResult, alignedSign, carryOut);
 
-    modport normal (input alignedResult, exponentOut, alignedSign, carryOut,
+    modport normal( input alignedResult, exponentOut, alignedSign, carryOut,
                     input A, B, exponentA, exponentB, mantissaA, mantissaB, signA, signB,
                     input Ainf, ANaN, Asub, Azero, Anormal,
                     input Binf, BNaN, Bsub, Bzero, Bnormal
                     output normalizedSign, normalizedExponent, normalizedMantissa);
 
-    modport pack (  input normalizedSign, normalizedExponent, normalizedMantissa,
+    modport pack(   input normalizedSign, normalizedExponent, normalizedMantissa,
                     output Result);
 
 
