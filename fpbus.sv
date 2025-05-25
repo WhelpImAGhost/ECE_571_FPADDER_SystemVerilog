714b6a64f353b19357a6c83d3dfaaf0f6b00060f
Violet Monti <76889116+WhelpImAGhost@users.noreply.github.com>
2025-05-25 00:18:26 -0700

    add sticky (again)

--- fpbus.sv
+++ fpbus.sv
@@ -10,7 +10,7 @@ interface fpbus;
     logic [22:0] mantissaA, mantissaB, normalizedMantissa;
     //Intermediate Results
     logic [31:0] alignedMantissaA, alignedMantissaB, alignedResult;
-    logic carryOut;
+    logic carryOut, sticky;
     //Control Signals
     logic Ainf, Binf, ANaN, BNaN, Asub, Bsub, Azero, Bzero, Anormal, Bnormal;
     logic shiftOverflow, bypassALU, Aex, Bex;
@@ -27,7 +27,7 @@ interface fpbus;
                     input Ainf, ANaN, Asub, Azero, Anormal,
                     input Binf, BNaN, Bsub, Bzero, Bnormal,
                     output alignedMantissaA, alignedMantissaB,
-                    output exponentOut,
+                    output exponentOut, sticky,
                     output shiftOverflow, bypassALU, Aex, Bex);
 
     modport alu(    input signA, signB,
@@ -39,6 +39,7 @@ interface fpbus;
                     input A, B, exponentA, exponentB, mantissaA, mantissaB, signA, signB,
                     input Ainf, ANaN, Asub, Azero, Anormal,
                     input Binf, BNaN, Bsub, Bzero, Bnormal,
+                    input sticky, 
                     output normalizedSign, normalizedExponent, normalizedMantissa);
 
     modport pack(   input normalizedSign, normalizedExponent, normalizedMantissa,
