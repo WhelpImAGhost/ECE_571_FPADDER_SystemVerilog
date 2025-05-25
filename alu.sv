6563406a495922cf73576f4bd22f1140d911977e
chriskp-pdx <chrisk@pdx.edu>
2025-05-24 21:10:29 -0700

    fixed syntax issues

--- alu.sv
+++ alu.sv
@@ -1,8 +1,5 @@
 //Module to Add Aligned Values Together
 module ALU(fpbus.alu bus);
-
-    logic [31:0] extendedResult;
-
     always_comb 
     begin
         //Defaults
@@ -14,19 +11,19 @@ module ALU(fpbus.alu bus);
         begin
             if (bus.signA == bus.signB)
             begin
-                {bus.carryOut, bus.alignedResult} = bus.alignedMantissaA + alignedMantissaB;
+                {bus.carryOut, bus.alignedResult} = bus.alignedMantissaA + bus.alignedMantissaB;
                 bus.alignedSign = bus.signA;
             end
             else 
             begin
                 if (bus.alignedMantissaB > bus.alignedMantissaA)
                 begin
-                    bus.alignedResult = bus.alignedMantissaA - alignedMantissaB;
+                    bus.alignedResult = bus.alignedMantissaA - bus.alignedMantissaB;
                     bus.alignedSign = bus.signB;
                 end
                 else
                 begin
-                    bus.alignedResult = bus.alignedMantissaA - alignedMantissaB;
+                    bus.alignedResult = bus.alignedMantissaA - bus.alignedMantissaB;
                     bus.alignedSign = bus.signA;
                 end
             end
@@ -40,7 +37,6 @@ module ALU(fpbus.alu bus);
             $display("\nMODULE ALU---------------------------");
             $display("Input Mantissas- A: %h (%b) B: %h (%b) ", bus.alignedMantissaA, bus.alignedMantissaA, bus.alignedMantissaB, bus.alignedMantissaB);
             $display("Input Exponents- A: %b (%0d) B: %b (%0d) ", bus.exponentA, bus.exponentA, bus.exponentB, bus.exponentB);
-            $display("Intermediate Result: %h (%b) ", extendedResult, extendedResult);
             $display("Aligned Result: %h (%b) ", bus.alignedResult, bus.alignedResult);
             $display("Carry Out: %0b", bus.carryOut);
             $display("Aligned Sign: %0b\n", bus.alignedSign);
--- pack.sv
+++ pack.sv
@@ -1,5 +1,4 @@
 //Module to Repack Result into IEEE-754 Standardized Format
-
 module Pack(fpbus.pack bus);
     assign bus.Result = {bus.normalizedSign, bus.normalizedExponent, bus.normalizedMantissa};
 endmodule
